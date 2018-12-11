21c39e4b72c4815a6abf5f4b8b1e62ae95e0cbfc
Manuel Kieweg <mail@manuelkieweg.de>
2018-12-11 15:23:39 +0000

    Audio Source gets spectrum data

---
+++ Reimagined Barnacle/Assets/_Scripts/Audio.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Audio : MonoBehaviour
+{
+	private AudioSource _audioSource;
+	public float[] Samples = new float[512]; 
+
+	// Use this for initialization
+	void Start ()
+	{
+		_audioSource = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GetSpectrumData();
+	}
+
+	void GetSpectrumData()
+	{
+		_audioSource.GetSpectrumData(Samples, 0, FFTWindow.Blackman);
+	}
+}
