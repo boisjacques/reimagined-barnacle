18a215e8e10c192b455cb3c6123ef7715e9ecbeb
Manuel Kieweg <mail@manuelkieweg.de>
2018-12-15 13:38:11 +0000

    Menu prototype

---
+++ Reimagined Barnacle/Assets/_Scripts/Menu.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace _Scripts
+{
+    public class Menu : MonoBehaviour
+    {
+        public void PlayGame()
+        {
+            SceneManager.LoadScene("Main Scene");
+        }
+
+        public void QuitGame()
+        {
+            Application.Quit();
+        }
+    }
+}
+
